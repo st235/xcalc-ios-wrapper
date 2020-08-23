f98b986e7bd96fbdf87460243c71c1627802294c
Alexander Dadukin <hi@st235.xyz>
2020-08-23 09:08:54 +0300

    Initial Commit

---
+++ XCalcWrapper/XCalcWrapper.h
@@ -0,0 +1,19 @@
+//
+//  XCalcWrapper.h
+//  XCalcWrapper
+//
+//  Created by Alexander Dadukin on 23.08.2020.
+//  Copyright © 2020 Alexander Dadukin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XCalcWrapper.
+FOUNDATION_EXPORT double XCalcWrapperVersionNumber;
+
+//! Project version string for XCalcWrapper.
+FOUNDATION_EXPORT const unsigned char XCalcWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XCalcWrapper/PublicHeader.h>
+
+
